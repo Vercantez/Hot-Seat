264ac34369728539a660b07b6972222e9059b2bf
Miguel Salinas <miguel.salinas@match.com>
2019-03-27 18:05:01 -0500

    Initial Commit

---
+++ HotSeat/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  HotSeat
+//
+//  Created by Miguel Salinas on 3/27/19.
+//  Copyright © 2019 Miguel Salinas. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
