0c7f34d4f954ca50d9ab4a1606e394ca06a574da
Dhruv Sangvikar <dhruv.sangvikar@match.com>
2019-03-28 11:31:11 -0500

    Add support for group video chat

--- HotSeat/DatingRoomViewController.swift
+++ HotSeat/DatingRoomViewController.swift
@@ -9,124 +9,156 @@
 import UIKit
 import OpenTok
 
-class DatingRoomViewController: UIViewController {
+var kApiKey = "46295492"
+var kSessionId = "2_MX40NjI5NTQ5Mn5-MTU1MzczMDg3NjAxNH5uS0I0L0tXQWNPN0R1NjlPSy9iTUZmOUN-fg"
+var kToken = "T1==cGFydG5lcl9pZD00NjI5NTQ5MiZzaWc9MDA1OWM1NzlhM2U2NzEyNjU4YWY2YmM3MDJiM2E3MDIzNTAwZmU0NzpzZXNzaW9uX2lkPTJfTVg0ME5qSTVOVFE1TW41LU1UVTFNemN6TURnM05qQXhOSDV1UzBJMEwwdFhRV05QTjBSMU5qbFBTeTlpVFVabU9VTi1mZyZjcmVhdGVfdGltZT0xNTUzNzg5ODI0Jm5vbmNlPTAuMjA2NjQ4MjI0OTAwNjI5NCZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTU2MzgxODIyJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9"
+
+class DatingRoomViewController: UICollectionViewController {
+    lazy var session: OTSession = {
+        return OTSession(apiKey: kApiKey, sessionId: kSessionId, delegate: self)!
+    }()
     
-    var session: OTSession?
-    var publisher: OTPublisher?
-    var subscriber: OTSubscriber?
+    lazy var publisher: OTPublisher = {
+        let settings = OTPublisherSettings()
+        settings.name = UIDevice.current.name
+        return OTPublisher(delegate: self, settings: settings)!
+    }()
     
-    
-    var kApiKey = "46295492"
-    var kSessionId = "2_MX40NjI5NTQ5Mn5-MTU1MzczMDg3NjAxNH5uS0I0L0tXQWNPN0R1NjlPSy9iTUZmOUN-fg"
-    var kToken = "T1==cGFydG5lcl9pZD00NjI5NTQ5MiZzaWc9NGI3ZDliMzNkMmIyMzBlY2E2MGM1MmJkY2EyMDExZjJhZTM3MTY1NjpzZXNzaW9uX2lkPTJfTVg0ME5qSTVOVFE1TW41LU1UVTFNemN6TURnM05qQXhOSDV1UzBJMEwwdFhRV05QTjBSMU5qbFBTeTlpVFVabU9VTi1mZyZjcmVhdGVfdGltZT0xNTUzNzM0NTUwJm5vbmNlPTAuODk0MzYwNTU1MDAwNTIxNiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTU2MzI2NTQ5JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9"
+    var subscribers: [OTSubscriber] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        switch AVCaptureDevice.authorizationStatus(for: .video) {
-        case .authorized: // The user has previously granted access to the camera.
-            self.connectToAnOpenTokSession()
-            
-        case .notDetermined: // The user has not yet been asked for camera access.
-            AVCaptureDevice.requestAccess(for: .video) { granted in
-                if granted {
-                    self.connectToAnOpenTokSession()
-                }
-            }
-            
-        case .denied: // The user has previously denied access.
-            return
-        case .restricted: // The user can't grant access due to restrictions.
-            return
+        doConnect()
+    }
+    
+    fileprivate func doConnect() {
+        var error: OTError?
+        defer {
+            processError(error)
+        }
+        session.connect(withToken: kToken, error: &error)
+    }
+    
+    fileprivate func doPublish() {
+        var error: OTError?
+        defer {
+            processError(error)
+        }
+        session.publish(publisher, error: &error)
+        
+        collectionView?.reloadData()
+    }
+    
+    fileprivate func doSubscribe(_ stream: OTStream) {
+        var error: OTError?
+        defer {
+            processError(error)
+        }
+        guard let subscriber = OTSubscriber(stream: stream, delegate: self)
+            else {
+                print("Error while subscribing")
+                return
+        }
+        session.subscribe(subscriber, error: &error)
+        subscribers.append(subscriber)
+        collectionView?.reloadData()
+    }
+    
+    fileprivate func cleanupSubscriber(_ stream: OTStream) {
+        subscribers = subscribers.filter { $0.stream?.streamId != stream.streamId }
+        collectionView?.reloadData()
+    }
+    
+    fileprivate func processError(_ error: OTError?) {
+        if let err = error {
+            showAlert(errorStr: err.localizedDescription)
         }
     }
     
-    func connectToAnOpenTokSession() {
-        session = OTSession(apiKey: kApiKey, sessionId: kSessionId, delegate: self)
-        var error: OTError?
-        session?.connect(withToken: kToken, error: &error)
-        if error != nil {
-            print(error!)
+    fileprivate func showAlert(errorStr err: String) {
+        DispatchQueue.main.async {
+            let controller = UIAlertController(title: "Error", message: err, preferredStyle: .alert)
+            controller.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(controller, animated: true, completion: nil)
         }
+    }
+    
+    // MARK: - UICollectionView methods
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return subscribers.count + 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "videoCell", for: indexPath)
+        let videoView: UIView? = {
+            if (indexPath.row == 0) {
+                return publisher.view
+            } else {
+                let sub = subscribers[indexPath.row - 1]
+                return sub.view
+            }
+        }()
+        
+        if let viewToAdd = videoView {
+            viewToAdd.frame = cell.bounds
+            cell.addSubview(viewToAdd)
+        }
+        return cell
     }
 }
 
+// MARK: - OTSession delegate callbacks
 extension DatingRoomViewController: OTSessionDelegate {
     func sessionDidConnect(_ session: OTSession) {
-        print("The client connected to the OpenTok session.")
-        let settings = OTPublisherSettings()
-        settings.name = UIDevice.current.name
-        guard let publisher = OTPublisher(delegate: self, settings: settings) else {
-            return
-        }
-        
-        var error: OTError?
-        session.publish(publisher, error: &error)
-        guard error == nil else {
-            print(error!)
-            return
-        }
-        
-        guard let publisherView = publisher.view else {
-            return
-        }
-        let screenBounds = UIScreen.main.bounds
-        publisherView.frame = CGRect(x: 0, y: 0, width: 150, height: 150)
-        view.addSubview(publisherView)
+        print("Session connected")
+        doPublish()
     }
     
     func sessionDidDisconnect(_ session: OTSession) {
-        print("The client disconnected from the OpenTok session.")
-    }
-    
-    func session(_ session: OTSession, didFailWithError error: OTError) {
-        print("The client failed to connect to the OpenTok session: \(error).")
+        print("Session disconnected")
     }
     
     func session(_ session: OTSession, streamCreated stream: OTStream) {
-        print("A stream was created in the session.")
-        subscriber = OTSubscriber(stream: stream, delegate: self)
-        guard let subscriber = subscriber else {
-            return
-        }
-        
-        var error: OTError?
-        session.subscribe(subscriber, error: &error)
-        guard error == nil else {
-            print(error!)
-            return
-        }
-        
-        guard let subscriberView = subscriber.view else {
-            return
-        }
-        
-        subscriberView.frame = CGRect(x: 0, y: 150, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - 150)
-        view.insertSubview(subscriberView, at: 0)
+        print("Session streamCreated: \(stream.streamId)")
+        doSubscribe(stream)
     }
     
     func session(_ session: OTSession, streamDestroyed stream: OTStream) {
-        print("A stream was destroyed in the session.")
-    }
-}
-
-extension DatingRoomViewController: OTPublisherDelegate {
-    func publisher(_ publisher: OTPublisherKit, didFailWithError error: OTError) {
-        print("The publisher failed: \(error)")
-    }
-}
-
-extension DatingRoomViewController: OTSubscriberDelegate {
-    public func subscriberDidConnect(toStream subscriber: OTSubscriberKit) {
-        print("The subscriber did connect to the stream.")
+        print("Session streamDestroyed: \(stream.streamId)")
+        cleanupSubscriber(stream)
     }
     
-    public func subscriber(_ subscriber: OTSubscriberKit, didFailWithError error: OTError) {
-        print("The subscriber failed to connect to the stream.")
+    func session(_ session: OTSession, didFailWithError error: OTError) {
+        print("session Failed to connect: \(error.localizedDescription)")
     }
 }
 
+// MARK: - OTPublisher delegate callbacks
+extension DatingRoomViewController: OTPublisherDelegate {
+    func publisher(_ publisher: OTPublisherKit, streamCreated stream: OTStream) {
+    }
+    
+    func publisher(_ publisher: OTPublisherKit, streamDestroyed stream: OTStream) {
+    }
+    
+    func publisher(_ publisher: OTPublisherKit, didFailWithError error: OTError) {
+        print("Publisher failed: \(error.localizedDescription)")
+    }
+}
 
+// MARK: - OTSubscriber delegate callbacks
+extension DatingRoomViewController: OTSubscriberDelegate {
+    func subscriberDidConnect(toStream subscriberKit: OTSubscriberKit) {
+        print("Subscriber connected")
+    }
+    
+    func subscriber(_ subscriber: OTSubscriberKit, didFailWithError error: OTError) {
+        print("Subscriber failed: \(error.localizedDescription)")
+    }
+    
+    func subscriberVideoDataReceived(_ subscriber: OTSubscriber) {
+    }
+}
---
+++ HotSeat/SubscriberViewUIComponents/SubscriberCollectionCell.swift
@@ -0,0 +1,9 @@
+//
+//  SubscriberCollectionCell.swift
+//  HotSeat
+//
+//  Created by Dhruv Sangvikar on 3/28/19.
+//  Copyright © 2019 Miguel Salinas. All rights reserved.
+//
+
+import Foundation
